9465617c05f6c895308ab25498ac3d65fbe6150e
canderson3522 <christopher.anderson3522@gmail.com>
2022-01-04 19:53:10 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { AuthenticatorComponent } from 'src/app/tools/authenticator/authenticator.component';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
+import { Router } from '@angular/router'
+import { FirebaseTSFirestore } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +12,58 @@ import { AuthenticatorComponent } from 'src/app/tools/authenticator/authenticato
 })
 export class AppComponent {
   title = 'ChitChat';
+  auth= new FirebaseTSAuth();
+  fireStore = new FirebaseTSFirestore();
+  userHasProfile = true;
 
-constructor(private loginSheet: MatBottomSheet){
-  
+constructor(private loginSheet: MatBottomSheet,
+    private router: Router
+  ){
+    this.auth.listenToSignInStateChanges(
+      user => {
+        this.auth.checkSignInState(
+          {
+            whenSignedIn: user => {
+              alert("Logged In");
+              this.isLoggedIn = true;
+            },
+            whenSignedOut: user => {
+              alert("Logged Out")
+            },
+            whenSignedInAndEmailNotVerified: user => {
+              this.router.navigate(["emailVerification"]);
+            },
+            whenSignedInAndEmailVerified: user => {
+              this.getUserProfile();
+            },
+            whenChanged: user => {
+
+            }
+          }
+        )
+      }
+    );
 }
+
+  getUserProfile(){
+    this.fireStore.listenToDocument(
+      {
+        name: "Getting Documnets",
+        path: ["Users", this.auth.getAuth().currentUser.uid],
+        onUpdate: (results) => {
+          this.userHasProfile = result.exists;
+        }
+      }
+    );
+  }
+
+  onLogoutClick(){
+    this.auth.signOut();
+  }
+
+  loggedIn(){
+    return this.auth.isSignedIn();
+  }
 
   onLoginClick() {
     this.loginSheet.open(AuthenticatorComponent);
